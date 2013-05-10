95ae0ebb029685a32df992be569732274a8e67bc
Rulo <solorulo@hotmail.com>
2013-05-09 21:48:49 -0500

    Inicio

---
+++ nzuri/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render_to_response
+import datetime
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	return render_to_response('current_datetime.html', {'current_date': now})
+def hours_ahead(request, offset):
+    #offset = int(offset)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
