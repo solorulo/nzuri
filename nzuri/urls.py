a948a7270139b3019b51f1090d0fab5fe7dbbed0
Rulo <solorulo@hotmail.com>
2013-05-12 16:13:32 -0500

    puras mamadas aun

--- nzuri/urls.py
+++ nzuri/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-from nzuri.views import current_datetime, hours_ahead
+from nzuri.views import current_datetime, hours_ahead, index
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'nzuri.views.home', name='home'),
+    url(r'^$', index),
     # url(r'^nzuri/', include('nzuri.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- nzuri/views.py
+++ nzuri/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 import datetime
 
 def current_datetime(request):
 	now = datetime.datetime.now()
 	return render_to_response('current_datetime.html', {'current_date': now})
 def hours_ahead(request, offset):
-    #offset = int(offset)
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+	#offset = int(offset)
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+	return HttpResponse(html)
+def index(request):
+	html = "<html><body>HOME.</body></html>"
+	return HttpResponse(html)
